12f5ee4a59f860bb2c29c8ce8b609f6d1e7a12f6
Juan Sebastian Esper <juansebastianesper@gmail.com>
2026-08-29 14:56:54 -0500

    u

--- api/main.py
+++ api/main.py
@@ -4,12 +4,7 @@ import httpx
 
 app = FastAPI()
 
-# URL de tu servidor en Listen2MyRadio
 RADIO_URL = "http://82.145.41.50:12274/stream"
-
-@app.get("/")
-async def root():
-    return {"message": "El puente de la radio está activo 🚀"}
 
 @app.get("/api/main")
 async def get_audio_stream():
